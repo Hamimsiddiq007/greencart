9b8a2011d679cf924cad4c904ff5010a635546f1
Hamim <hamimsiddiq915@gmail.com>
2026-04-30 22:58:09 +0600

    structure

---
+++ client/src/context/AppContextProvider.jsx
@@ -0,0 +1,20 @@
+import { useContext, useState } from "react";
+import { AppContext } from "./AppContext";
+import { useNavigate } from "react-router-dom";
+
+const AppContextProvider = ({ children }) => {
+
+    const navigate = useNavigate()
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+
+    const value = {navigate, user, setUser, isSeller, setIsSeller};
+    
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+}

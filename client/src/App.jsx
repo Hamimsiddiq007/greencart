9bb281de47cdabecac51d12696afcfc23d2d9c11
Hamim <hamimsiddiq915@gmail.com>
2026-05-01 18:13:25 +0600

    feat: navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='text-red-600'>
-      hello
+    <div>
+      <Navbar/>
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AppContextProvider } from './context/AppContextProvider.jsx'
+import  AppContextProvider from './context/AppContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
